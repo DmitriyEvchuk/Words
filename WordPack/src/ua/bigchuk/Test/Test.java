bada0dda488e95c549dd4d77e8c7a9187905c9d9
DmitriyEvchuk <dmitriy.evchuk@gmail.com>
2015-03-24 08:20:47 +0200

    With timer

--- WordPack/src/ua/bigchuk/Test/Test.java
+++ WordPack/src/ua/bigchuk/Test/Test.java
@@ -6,19 +6,58 @@ abstract class Test {
 
 	public static void main(String[] args) {
 
+		// for total time
+		long totalT = -System.currentTimeMillis();
+
 		ImplementWordsExtraction wordsextractor = new ImplementWordsExtraction();
 		WordsCounterImplement countWords = new WordsCounterImplement();
+
+		// for file read time
+		long readT = -System.currentTimeMillis();
+
 		FileReaderImplement file = new FileReaderImplement(
 				"E:\\work\\rfc2822.txt");
-		WordSaveImplement fileSave = new WordSaveImplement("forSun.txt");
 
 		file.readFile();
 
-		wordsextractor.parseString(file.getFileString());
+		String fileString = file.getFileString();
 
-		countWords.wordsCounter(wordsextractor.getWords());
+		// for file read time
+		readT += System.currentTimeMillis();
 
+		// for word extract time
+		long extractT = -System.currentTimeMillis();
+
+		wordsextractor.parseString(fileString);
+		String[] f = wordsextractor.getWords();
+
+		// for word extract time
+		extractT += System.currentTimeMillis();
+
+		// for word counter time
+		long countT = -System.currentTimeMillis();
+
+		countWords.wordsCounter(f);
+
+		// //for word counter time
+		countT += System.currentTimeMillis();
+
+		// for save time
+		long saveT = -System.currentTimeMillis();
+
+		WordSaveImplement fileSave = new WordSaveImplement("forSun.txt");
 		fileSave.save(countWords);
+
+		// for save time
+		saveT += System.currentTimeMillis();
+		// for total time
+		totalT += System.currentTimeMillis();
+
+		System.out.println("total " + totalT);
+		System.out.println("read " + readT);
+		System.out.println("extract " + extractT);
+		System.out.println("count " + countT);
+		System.out.println("save " + saveT);
 
 	}// main
 
